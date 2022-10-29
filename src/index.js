2a2886dc53a78ff4127ecbefcb9fbb57c0fa5550
GleidsonLM <gleidsonlm@gmail.com>
2022-10-29 06:33:41 -0300

    add: update repository

--- src/index.js
+++ src/index.js
@@ -6,15 +6,26 @@ const app = express();
 
 app.use(express.json());
 
-const repositories = [];
 
+const repositories = [];
+/* repositories Model
+  {
+  id: uuid(), //primary unique uuid
+  title, //string
+  url, //string
+  techs, //array
+  likes: 0 //integer autoincrement !important starts with 0
+  }
+*/
+
+// list the projects
 app.get("/repositories", (request, response) => {
-  return response.json(repositories);
+  return response.status(200).json(repositories);
 });
 
+// create a new repository
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
-
   const repository = {
     id: uuid(),
     title,
@@ -22,26 +33,33 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
-  return response.json(repository);
+  
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
+// update repository
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
-
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
+  const newRepository = request.body;
+  const repositoryIndex = repositories.findIndex(e => e.id === id);
+  let repository;
+  // don't update a non existing repository
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
+  } else {
+    repository = repositories[repositoryIndex];
+    if (!newRepository.title){
+      repository.title = newRepository.title;
+    } else if (!newRepository.url) {
+      repository.url = newRepository.url;
+    } else if (!newRepository.techs) {
+      repository.url = newRepository.url;
+    }
   }
-
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
-});
+    return response.status(200).json(repository);
+  }
+);
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
